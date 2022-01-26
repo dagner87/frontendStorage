1ab8a92e4eb07e2d9ad1176066ffd5f36b60b303
Dagner Alena Guerra <dalenag87@gmail.com>
2022-01-25 23:18:27 -0500

    Update productos.service.ts

--- src/app/shared/service/productos.service.ts
+++ src/app/shared/service/productos.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { ProductoItems } from '../interfaces/producto.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductosService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  obtenerProductosPaginados(): Observable<ProductoItems> {
+    return this.http.get<ProductoItems>(`${environment.base_url}productos`);
+  }
 }
